8e99d426b25ae3535dfff5938691a02325727a75
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2021-04-05 23:09:52 -0700

    changes

--- app/Observers/productObserver.php
+++ app/Observers/productObserver.php
@@ -9,7 +9,7 @@ class productObserver
     public function updating(product $product)
     {
      if ($product->Qty <= 0) {
-        $product->Status = 'Not Available';}
+        $product->Status = 'Out of Stock';}
 
      else if ($product->Qty <= 5 && $product->Qty >= 1){
         $product->Status='Reorder level';}
